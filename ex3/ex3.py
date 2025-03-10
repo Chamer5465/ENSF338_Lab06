72e540734391f6e0263d83f9aa97902a48203a5b
Chamer5465 <chasemack@shaw.ca>
2025-03-10 11:54:40 -0600

    Ex 3 started

---
+++ ex3/ex3.py
@@ -0,0 +1,25 @@
+class TreeNode:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+    def set_right(self, right):
+        self.right = right
+    
+    def set_left(self, left):
+        self.left = left
+
+    def get_right(self):
+        return self.right
+    
+    def get_left(self):
+        return self.left
+
+def main():
+    
+
+if __name__ == '__name__':
+    main()
+
+
