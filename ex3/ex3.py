38b6a2f5dfb74721bc9815dfdaaf85f382954480
Chamer5465 <chasemack@shaw.ca>
2025-03-11 14:50:07 -0600

    Updated ex3

--- ex3/ex3.py
+++ ex3/ex3.py
@@ -1,8 +1,8 @@
 from sys import argv
 
 class TreeNode:
-    def __init__(self, data):
-        self.data = data
+    def __init__(self):
+        self.data = None
         self.left = None
         self.right = None
 
@@ -17,12 +17,38 @@ class TreeNode:
     
     def get_left(self):
         return self.left
+    
+    def insert(self, data):
+        if '(' not in data:
+            self.data = int(data)
+        else:
+            data_list = data.split(' ')
+            count = 0
+            for j, i in enumerate(data_list):
+                if i == '(' and j > 0:
+                    count += 1
+                elif i == ')':
+                    count -= 1
+                elif i.isdigit() == False and count == 0:
+                    self.data = i
+                    if ''.join(data_list[1:j]).isdigit():
+                        self.left = data_list[1:j].join()
+                    else:
+                        self.insert(' '.join(data_list[1:j]))
+                    if ''.join(data_list[j + 1:-1]).isdigit():
+                        self.left = data_list[j + 1:-1].join()
+                    else:
+                        self.insert(' '.join(data_list[j + 1:-1]))
+                    
+                
+            
 
 def main():
-    data = argv[1].replace(' ', '')
-    indexes = []
-    result = []
-    for j, e in enumerate(data):
+    data = argv[1]
+    root = TreeNode()
+    root.insert(data)
+    print('Insert Completed')
+    """for j, e in enumerate(data):
         if e == ')':
             end_index = j
             for i in range(end_index, -1, -1):
@@ -32,7 +58,7 @@ def main():
                     break
             result.append(data[start_index + 1: end_index])
     print(result)
-
+"""
     
 
 if __name__ == '__main__':
