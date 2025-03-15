70da8473d892e52c1652b7cc96495dadefa7ab41
Chamer5465 <chasemack@shaw.ca>
2025-03-15 15:44:05 -0600

    Ex2 finalized

--- ex2/ex2.py
+++ ex2/ex2.py
@@ -60,16 +60,15 @@ def partition(arr, low, high):
     return right
 
 def binarySearch(arr, low, high, x):
-    if high >= low:
+    while low <= high:
         mid = low + (high - low) // 2
         if arr[mid] == x:
             return mid
-        elif arr[mid] > x:
-            return binarySearch(arr, low, mid-1, x)
+        elif arr[mid] < x:
+            low = mid + 1
         else:
-            return binarySearch(arr, mid + 1, high, x)
-    else:
-        return -1
+            high = mid - 1
+    return None
 
 def main():
     theList = [x for x in range(10000)]
@@ -84,25 +83,24 @@ def main():
     for i in theList:
         totalTimeBST = 0
         totalTimeBS = 0
-        for j in range(10):
-            totalTimeBST += timeit.timeit(lambda: search(i, root))
-            totalTimeBS += timeit.timeit(lambda: binarySearch(theVector, 0, 9999, i))
+        totalTimeBST += timeit.timeit(lambda: search(i, root), number=10)
+        totalTimeBS += timeit.timeit(lambda: binarySearch(theVector, 0, 9999, i), number=10)
         averageTimeBST = totalTimeBST / 10
         averageTimeBS = totalTimeBS / 10
         grandTotalTimeBST += totalTimeBST
         grandTotalTimeBS += totalTimeBS
-        print(f'Total time to search for {i} 10 times in a binary search tree: {totalTimeBST:.3f}')
-        print(f'Average time to search for {i} in a binary search tree: {averageTimeBST:.3f}')
-        print(f'Total time to binary search for {i} 10 times in a vector is: {totalTimeBS:.3f}')
-        print(f'Average time to binary search for {i} in a vector is: {averageTimeBS:.3f}')
+        print(f'Total time to search for {i} 10 times in a binary search tree: {totalTimeBST}')
+        print(f'Total time to binary search for {i} 10 times in a vector is: {totalTimeBS}')
+        print(f'Average time to search for {i} in a binary search tree: {averageTimeBST}')
+        print(f'Average time to binary search for {i} in a vector is: {averageTimeBS}')
     grandAverageTimeBST = grandTotalTimeBST / 100000
     grandAverageTimeBS = grandTotalTimeBS / 100000
     print(f'Total time to search for each element 10 times in a binary search tree: {grandTotalTimeBST}')
-    print(f'Average time to search for an element in a binary search tree: {grandAverageTimeBST}')
     print(f'Total time to binary search for each element 10 times in a vector: {grandTotalTimeBS}')
+    print(f'Average time to search for an element in a binary search tree: {grandAverageTimeBST}')
     print(f'Average time to binary search for an element in a vector: {grandAverageTimeBS}')
     
-#4. Searching through a binary search tree is faster. This is because in a binary search tree, nodes are arranged in way that causes each comparison to skip around half of whats of the tree. Binary search does the same, but it requires the middle element to be the value we are searching for, leading to it having to make more comparisons.
+#4. Binary search takes about 2 times as long as searching a binary search tree. This seems odd because the two search algorithms do the same thing, however a binary search requires some math and array indexing, which takes longer than simply accessing data the tree nodes.
     
 if __name__ == '__main__':
     main()
